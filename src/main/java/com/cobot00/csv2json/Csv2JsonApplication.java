08b50abd2b4186c36b6039152dcd8563defcd843
Ryoji Kobori <kobori75@gmail.com>
2016-08-22 01:30:44 +0900

    init

---
+++ src/main/java/com/cobot00/csv2json/Csv2JsonApplication.java
@@ -0,0 +1,12 @@
+package com.cobot00.csv2json;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Csv2JsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Csv2JsonApplication.class, args);
+	}
+}
---
+++ src/test/java/com/cobot00/csv2json/Csv2JsonApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cobot00.csv2json;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Csv2JsonApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
