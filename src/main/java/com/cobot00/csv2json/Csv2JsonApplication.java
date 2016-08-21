05563ffe846a3b8ade24ba5120ff2c9fa07e6963
Ryoji Kobori <kobori75@gmail.com>
2016-08-22 03:10:52 +0900

    implement run

--- src/main/java/com/cobot00/csv2json/Csv2JsonApplication.java
+++ src/main/java/com/cobot00/csv2json/Csv2JsonApplication.java
@@ -2,11 +2,18 @@ package com.cobot00.csv2json;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Csv2JsonApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Csv2JsonApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(Csv2JsonApplication.class, args);
+        Csv2JsonApplication app = context.getBean(Csv2JsonApplication.class);
+        app.run();
+    }
+
+    public void run() {
+        System.exit(0);
+    }
 }
